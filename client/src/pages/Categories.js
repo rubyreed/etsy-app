a7ec8722cdc939e3ff6acd6a40ff23326cb2c450
Ruby Reed <rubysreed@gmail.com>
2022-01-07 14:20:01 -0700

    categories organized

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { Header, Table, Divider, Container, Dropdown } from "semantic-ui-react";
+import { Header, Table, Divider, Container, Card } from "semantic-ui-react";
 import axios from "axios";
 
 const Categories = () => {
@@ -10,40 +10,75 @@ useEffect(() => {
   getCategories();
 },[]);
 
+const normalizeData = (data)=>{
+  let	categories = data.map( c => c.category) //goes through and finds category for each thing in database(unique or not)
+  let uniqueCategories = [... new Set(categories)]//makes sure all categories are unique
+
+  let normalizedData = uniqueCategories.map(unique_category =>{
+  let products =  data.filter( d=> d.category === unique_category) //finding all products for each individual category
+  let filterCategories = products.map(product=>{
+      return {key: product.id, category: product.category, price: product.price, description:product.description}
+  }) //here we take only the info that we want out of products
+  
+    return {
+        category:  products[0].category, //using first to be safe, don't know how many object there are in array
+        products: filterCategories
+    }
+  }) //for each unique seller we want to grab these things
+  return normalizedData
+};
 
 const getCategories = async () => {
+  try { 
   let response = await axios.get("/api/categories");
-  setCategories(response.data)
+  let normalizedData = normalizeData(response.data)
+  setCategories(normalizedData)
+} 
+  catch (error) {
+  alert ('error occured getCategories')
+}
 };
 console.log(categories)
 
-const renderRow = () => {
+const renderCategories = () => {
   return categories.map((category) => {
-    return(
-    <Table.Row>
-      <Table.Cell>{category.category}</Table.Cell>
-      <Table.Cell>$ {category.price}</Table.Cell>
-      <Table.Cell>{category.description}</Table.Cell>
-    </Table.Row> )
-  })
-}
-
-  return (
-    <Container>
-    <Header>Categories</Header>
-    <Table celled>
+    return (
+    <>
+      <Card>
+        <Card.Content>
+          <Card.Header>{category.category}</Card.Header>
+        </Card.Content>
+      </Card>
+      <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Category</Table.HeaderCell>
             <Table.HeaderCell>Price</Table.HeaderCell>
             <Table.HeaderCell>Description</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
   <Table.Body>
-    {renderRow()}
+    {renderRow(category.products)}
   </Table.Body>
 </Table>
 <Divider style={{marginTop:"50px", marginBottom:"50px"}}/>
+    </>
+    );
+  });
+  };
+
+const renderRow = (data) => {
+  return data.map((product) => {
+    return(
+    <Table.Row>
+      <Table.Cell>$ {product.price}</Table.Cell>
+      <Table.Cell>{product.description}</Table.Cell>
+    </Table.Row> )
+  })
+}
+  return (
+    <Container>
+    <Header as='h1' style={{marginBottom:'50px', marginTop:'30px'}}>Categories</Header>
+    {renderCategories()}
     </Container>
   )
 }
